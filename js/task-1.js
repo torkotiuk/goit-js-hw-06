22423e2f8336bf156c38bd44a956d8edadf3be6d
Andrii Torkotiuk <torkotiuk@gmail.com>
2021-01-14 20:45:07 +0100

    js6

---
+++ js/task-1.js
@@ -0,0 +1,7 @@
+import users from './users';
+
+const getUserNames = users => {
+  return users.map(user => user.name);
+};
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
